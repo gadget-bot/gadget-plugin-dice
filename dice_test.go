8068e770cf47c5ffd12f638e69913cd8c9080c41
Jonathan Gnagy <jonathan.gnagy@gmail.com>
2021-07-04 14:53:00 -0700

    chore: adding more tests

--- dice_test.go
+++ dice_test.go
@@ -8,3 +8,15 @@ func TestGetMentionRoutes(t *testing.T) {
 		t.Errorf("len(GetMentionRoutes()) = %d; want 1", len(ans))
 	}
 }
+
+func TestRollDie(t *testing.T) {
+	ans := rollDie([]int{1})
+	if ans != 1 {
+		t.Errorf("rollDie([]int{1}) = %d; want 1", ans)
+	}
+
+	ans = rollDie([]int{4, 5, 6})
+	if ans != 4 && ans != 5 && ans != 6 {
+		t.Errorf("rollDie([]int{4, 5, 6}) = %d; want one of [4, 5, 6]", ans)
+	}
+}
